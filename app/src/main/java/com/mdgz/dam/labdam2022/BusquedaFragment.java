2d7188ccc9ca7c91ac7fe84ddea9bde13a8f261a
clari62 <clariespertino62@gmail.com>
2022-09-23 20:53:13 -0300

    Trabajo clase práctica 23/9

--- app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/BusquedaFragment.java
@@ -3,10 +3,15 @@ package com.mdgz.dam.labdam2022;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.mdgz.dam.labdam2022.databinding.FragmentBusquedaBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,7 @@ public class BusquedaFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FragmentBusquedaBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,6 +65,18 @@ public class BusquedaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_busqueda, container, false);
+        //View v = inflater.inflate(R.layout.fragment_busqueda, container, false);
+        binding = FragmentBusquedaBinding.inflate(inflater, container, false);
+
+        Button b = binding.buttonBuscar;
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("app", "onClick: ");
+                NavHostFragment.findNavController(BusquedaFragment.this).navigate(R.id.action_busquedaFragment_to_resultadoBusquedaFragment);
+            }
+        });
+
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -1,16 +1,25 @@
 package com.mdgz.dam.labdam2022;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.FrameLayout;
+
+import com.mdgz.dam.labdam2022.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    private ActivityMainBinding binding;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+
+        BusquedaFragment f_busqueda = new BusquedaFragment();
+        FragmentManager frg_mng = getSupportFragmentManager();
+        frg_mng.beginTransaction().add(R.id.fragmentContainerView, f_busqueda).addToBackStack(null).commit();
+    }
 }
