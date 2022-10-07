7835fe72d2e7e6eedc938e542e63322c846af73f
antoforestello <anto.forestello@gmail.com>
2022-10-07 18:21:57 -0300

    DatePicker

--- app/src/main/java/com/mdgz/dam/labdam2022/DetalleAlojamientoFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/DetalleAlojamientoFragment.java
@@ -1,16 +1,27 @@
 package com.mdgz.dam.labdam2022;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.mdgz.dam.labdam2022.databinding.FragmentBusquedaBinding;
 import com.mdgz.dam.labdam2022.databinding.FragmentDetalleAlojamientoBinding;
 import com.mdgz.dam.labdam2022.databinding.FragmentResultadoBusquedaBinding;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +35,12 @@ public class DetalleAlojamientoFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private FragmentDetalleAlojamientoBinding binding;
+    private EditText editFechaInicio;
+    private ImageButton pickInicio;
+    private EditText editFechaFin;
+    private ImageButton pickFin;
+    private Context ctx = this.getContext();
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -66,6 +83,63 @@ public class DetalleAlojamientoFragment extends Fragment {
         binding = FragmentDetalleAlojamientoBinding.inflate(inflater, container, false);
         Bundle data = getArguments();
         binding.tvDetalle.setText(data.getString("descripcion"));
+
+        editFechaInicio = binding.fechInicio;
+        pickInicio= binding.idBtnPickDate;
+
+        pickInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DATE);
+
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                            new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker view, int year,
+                                                      int monthOfYear, int dayOfMonth) {
+                                    editFechaInicio.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                                }
+                            },
+
+                            year, month, day);
+
+                    datePickerDialog.show();
+                }
+
+        });
+        editFechaFin = binding.fechFin;
+        pickFin= binding.idBtnPickDate2;
+
+        pickFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DATE);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                editFechaFin.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            }
+                        },
+
+                        year, month, day);
+
+                datePickerDialog.show();
+            }
+
+        });
+
         return binding.getRoot();
     }
+
+
 }
+
